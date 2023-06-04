ae282e51d8bf8b61f1b819a8d7e4eed2fe34a993
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-06-04 16:05:45 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,29 @@
- 
+ import React, { useState } from "react";
+import { searchMovieData } from "./api";
+
+const SearchBar = ({ setMovies }) => {
+    const [searchInput, setSearchInput] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        searchMovieData(searchInput).then((data) => {
+            let { results } = data;
+            console.log(results);
+            setMovies(results);
+        });
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                onChange={(e) => setSearchInput(e.target.value)}
+                value={searchInput}
+                type="text"
+                className="search-input"
+            />
+            <button type="submit" className="search-btn">
+                Search
+            </button>
+        </form>
+    );
+};
+
+export default SearchBar;
