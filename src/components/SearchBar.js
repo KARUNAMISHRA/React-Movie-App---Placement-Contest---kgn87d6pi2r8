d46f6c413a1102e56bcea921906c2069aa1a1db2
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-06-04 16:04:29 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,1 @@
-import React, { useState } from "react";
-
-const SearchBar = (props) => {
-    const [searchInput, setSearchInput] = useState("");
-    
-    return (
-        <form>
-            <input type="text" className="search-input"/>
-            <button type="submit" className="search-btn">Search</button>
-        </form>
-    );
-}
-
-export default SearchBar;
+ 
